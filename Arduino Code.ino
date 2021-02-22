9a604b2a1b5a3dd68b75b855bf9b4c036b68d500
SirArck <32362801+SirArck@users.noreply.github.com>
2021-02-22 15:32:07 -0400

    Ajuste de delays

--- Arduino Code.ino
+++ Arduino Code.ino
@@ -88,7 +88,7 @@ void setup() {
 //line 1 é linha na esquerda
 //line 2 é linha na direita
 //line 3 é linha dos dois lados
-int senliL, senliR, sendiL, sendiR, line = 0, blPart, rotacao = 1, strat = 0;
+int senliL, senliR, sendiL, sendiR, line = 0, blPart = 700, rotacao = 1, strat = 0;
 
 //obtem o valor dos dois sensores de distancia
 void SenDi(){
@@ -181,10 +181,10 @@ void loop() {
                     //a buscar o oponente
                     MotorL(255);
                     MotorR(255);
-                    delay(6);
+                    delay(20);
                     MotorL(-255);
                     MotorR(-255);
-                    delay(9);
+                    delay(30);
                     rotacao = -1; //gira no sentido horario quando busca o oponente
                     SenDi(); //distancia
                     SenLi(); //linha
@@ -195,7 +195,7 @@ void loop() {
                     //robô faz uma curva em 45° antes de começar a buscar o oponente
                     MotorL(64);
                     MotorR(255);
-                    delay(5);
+                    delay(15);
                     rotacao = -1; //gira no sentido horario quando busca o oponente
                     SenDi(); //distancia
                     SenLi(); //linha
@@ -225,7 +225,7 @@ void loop() {
                     //espelhamento da 7
                     MotorL(255);
                     MotorR(64);
-                    delay(5);
+                    delay(15);
                     rotacao = 1; //gira no sentido anti-horário quando busca o oponente
                     SenDi(); //distancia
                     SenLi(); //linha
@@ -235,10 +235,10 @@ void loop() {
                     //espelhamento da 6
                     MotorL(255);
                     MotorR(255);
-                    delay(7);
+                    delay(20);
                     MotorL(-255);
                     MotorR(-255);
-                    delay(8);
+                    delay(25);
                     rotacao = 1; //gira no sentido anti-horário quando busca o oponente
                     SenDi(); //distancia
                     SenLi(); //linha
@@ -248,10 +248,10 @@ void loop() {
                     //by Marco Przybysz
                     MotorL(255);
                     MotorR(255);
-                    delay(4);
+                    delay(10);
                     MotorL(255);
                     MotorR(-128);
-                    delay(8);
+                    delay(25);
                     rotacao = -1; //gira no sentido horario quando busca o oponente
                     SenDi(); //distancia
                     SenLi(); //linha
@@ -261,10 +261,10 @@ void loop() {
                     //espelhamento do 12
                     MotorL(255);
                     MotorR(255);
-                    delay(4);
+                    delay(10);
                     MotorL(-128);
                     MotorR(255);
-                    delay(8);
+                    delay(25);
                     rotacao = 1; //gira no sentido horario quando busca o oponente
                     SenDi(); //distancia
                     SenLi(); //linha
@@ -276,7 +276,7 @@ void loop() {
                     MotorL(255);
                     MotorR(255);
                     rotacao = -1; //gira no sentido horario quando busca o oponente
-                    delay(16);
+                    delay(50);
                     break;
 
                case 15: //B3: the ultimate suicidal robot
@@ -285,7 +285,7 @@ void loop() {
                     MotorL(255);
                     MotorR(255);
                     rotacao = 1; //gira no sentido anti-horário quando busca o oponente
-                    delay(16);
+                    delay(50);
                     break;
           }
           //a ideia é que depois da parte inicial da estratégia, a movimentação
@@ -432,12 +432,7 @@ void loop() {
      senliL = digitalRead(lineL);
      senliR = digitalRead(lineR);
      //define o valor no sensor de linha na parte que o robô pode andar
-     if(senliL <= senliR){
-          blPart = senliR;
-     }else{
-          blPart = senliL;
-     }
-     blPart = blPart - 300;
+
 
 }
 
